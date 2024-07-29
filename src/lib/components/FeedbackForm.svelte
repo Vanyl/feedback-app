16884e40b1fc00dee61e17064ee6ed4139ba65f7
Yavanna Perez <yavannaperez@hotmail.com>
2024-07-29 17:04:21 +0200

    basic form

---
+++ src/lib/components/FeedbackForm.svelte
@@ -0,0 +1,34 @@
+<script>
+</script>
+
+<form action="">
+    <!-- rating select -->
+    <div class="input-group">
+        <input
+            type="text"
+            placeholder="tell us something that keeps you coming back"
+        />
+        <button>send</button>
+    </div>
+</form>
+
+<style>
+    .input-group {
+        display: flex;
+        flex-direction: row;
+        border: 1px solid #ccc;
+        padding: 8px 10px;
+        border-radius: 8px;
+        margin-top: 15px;
+    }
+
+    input {
+        flex-grow: 2;
+        border: none;
+        font-size: 16px;
+    }
+
+    input:focus {
+        outline: none;
+    }
+</style>
