89bdfe69cc8d9c7ca21396ef77367bdb15a6588d
Yavanna Perez <yavannaperez@hotmail.com>
2024-07-30 01:27:49 +0200

    feedback form handle submit, selection and input

--- src/lib/components/FeedbackForm.svelte
+++ src/lib/components/FeedbackForm.svelte
@@ -1,18 +1,80 @@
 <script>
+    import {v4 as uuidv4} from 'uuid'
+    import { createEventDispatcher } from "svelte";
+    import Button from "./Button.svelte";
+    import Card from "./Card.svelte";
+    import RatingSelect from "./RatingSelect.svelte";
+
+    let text = "";
+    let rating = 10;
+    let btnDisabled = true;
+    let min = 10;
+    let message;
+    let dispatch = createEventDispatcher()
+
+    const handleSelect = e => rating = e.detail
+
+    const handleInput = () => {
+        if (text.trim().length <= min) {
+            message = `Text must be at leat ${min} characters`;
+            btnDisabled = true;
+        } else {
+            message = null;
+            btnDisabled = false;
+        }
+    };
+
+    const handleSubmit = () => {
+        if(text.trim().length > min) {
+            //create new object
+            const newFeedback = {
+                id: uuidv4(), //custom id
+                text,
+                rating: +rating
+            }
+            // console.log(newFeedback)
+            dispatch('add-feedback', newFeedback)
+
+            text = ''
+        }
+    }
 </script>
 
-<form action="">
-    <!-- rating select -->
-    <div class="input-group">
-        <input
-            type="text"
-            placeholder="tell us something that keeps you coming back"
-        />
-        <button>send</button>
-    </div>
-</form>
+<Card>
+    <header>
+        <h2>How would you rate our service with us ?</h2>
+    </header>
+    <form on:submit|preventDefault={handleSubmit}>
+       <RatingSelect on:rating-select={handleSelect} />
+        <div class="input-group">
+            <input
+                type="text"
+                on:input={handleInput}
+                bind:value={text}
+                placeholder="tell us something that keeps you coming back"
+            />
+            <Button type="submit" disabled={btnDisabled}>Send</Button>
+        </div>
+        {#if message}
+            <div class="message">
+                {message}
+            </div>
+        {/if}
+    </form>
+</Card>
 
 <style>
+    header {
+        max-width: 400px;
+        margin: auto;
+    }
+
+    header h2 {
+        font-size: 22px;
+        font-weight: 600;
+        text-align: center;
+    }
+
     .input-group {
         display: flex;
         flex-direction: row;
@@ -30,5 +92,11 @@
 
     input:focus {
         outline: none;
+    }
+
+    .message {
+        padding-top: 10px;
+        text-align: center;
+        color: rebeccapurple;
     }
 </style>
